503076c0c612a2ca1c208f5260ed9cc88576d4e8
Saakshig12 <saakshig12@gmail.com>
2019-08-26 12:22:15 -0400

    basic info for app.js added

---
+++ my-app/src/app.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import ImageCard from "./components/ImageCard";
+import Wrapper from "./components/Wrapper";
+import Title from "./components/Title";
+import images from "./images.json";
+
+class App extends Component {
+    state = {
+        images
+    }
+}; 
+
+shuffleImage = id => {
+    
+}
