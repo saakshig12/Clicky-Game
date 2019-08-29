5662c8ed55d7893810065c8bece7043d91963e81
Saakshig12 <saakshig12@gmail.com>
2019-08-28 23:11:38 -0400

    update

--- my-app/src/app.js
+++ my-app/src/app.js
@@ -6,47 +6,49 @@ import animals from "./Animals.json";
 
 
 class App extends Component {
-    state = {
-        animals,
-        clickId: [],
-        Score: 0,
-        current: "",
-    }
-    shuffleImage ()
-  };
+  state = {
+    animals,
+    clickId: [],
+    score: 0,
+    current: "",
+  }
+  shuffleCard = (id) => {
+    onClick
+    let shuffle = Math.floor(Math.random(0, 9) * 2);
+  }
 
-    render() {
-        return (
-          <div className="App">
-              <Navbar
-              count = {this.state.current}
-              score = {this.state.score}
-              />
-            <header className="App-header">
-              <h1 className="App-title">Clicky</h1>
-              <h2>Try not to click on an image twice!</h2>
-            </header>
-            {/* <Score total={this.state.score}
-                    status={this.state.status}
-                    /> */}
-            <Wrapper>
-              {this.state.images.map(images => {
-                return(
-                <AnimalCard
-                id={animals.id}
-                key={animals.id}
-                image={animals.image}
-                clickId={animals.clicked}
-                shuffleImage={animals.shuffleImage}
-                />
-                )
-    
-                })}
-            </Wrapper>
-          </div>
-        );
-    };
+};
+
+render() {
+  return (
+    <div className="App">
+      <Navbar
+        count={this.state.current}
+        score={this.state.score}
+      />
+      <header className="App-header">
+        <h1 className="App-title">Clicky</h1>
+        <h2>Try not to click on an image twice!</h2>
+      </header>
+
+      <Wrapper>
+        {this.state.images.map(images => {
+          return (
+            <AnimalCard
+              id={animals.id}
+              key={animals.id}
+              image={animals.image}
+              clickId={animals.clicked}
+              shuffleCard={image.shuffleCard}
+            />
+          )
+
+        })}
+      </Wrapper>
+    </div>
+  );
 }
 
-export default App; 
+
+export default App;
 
